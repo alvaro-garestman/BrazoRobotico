e1f266e1c3300007eb33b628bb296bc9e5de0a92
Alvaro <agarest1@alumno.upv.es>
2025-10-04 20:30:17 +0200

    Terminada por mi cuenta

--- BrazoRobotico.cpp
+++ BrazoRobotico.cpp
@@ -19,6 +19,21 @@
 		return z;
 	}
 	
-	bool
-
+	bool BrazoRobotico::getSujetando(){
+		return sujetando;
+	}
+	
+	void BrazoRobotico::coger(){
+		sujetando = true;
+	}
+	
+	void BrazoRobotico::soltar(){
+		sujetando = false;
+	}
+	
+	void BrazoRobotico::mover(double _x, double _y, double _z){
+		x = _x;
+		y = _y;
+		z = _z;
+	}
 
