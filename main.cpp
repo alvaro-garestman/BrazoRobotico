d6acbe10753f8e5fff69793326264f7d7a1af04d
Alvaro <agarest1@alumno.upv.es>
2025-10-04 20:38:21 +0200

    Terminada por mi cuenta

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
-sajsld
+#include "brazoRobico.h"
+#include <iostream>
+using namespace std
+
+int main (){
+	BrazoRobotico brazo;
+
+	brazo.mover(2.0, 4.0, 1.0);
+	brazo.coger();
+	cout <<"Esta sujetando un objeto? =>" << brazo.getSujetando() << endl;
+	cout <<"Posicion actual: X " << brazo.getX() << "Y: " << brazo.getY() << "Z: " << brazo.getZ() << endl;
+
+	return 0;
+}
+
+	
