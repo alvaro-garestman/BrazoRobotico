a5257a577590a97ef2406e951b34b87788b72faa
Alvaro <agarest1@alumno.upv.es>
2025-10-04 20:19:25 +0200

    No he definido el getsujetando

--- BrazoRobotico.cpp
+++ BrazoRobotico.cpp
@@ -1,3 +1,24 @@
-#include <brazoRobotico.h>
+#include "brazoRobico.h"
+
+	BrazoRobotico::BrazoRobotico(){
+		x = 0;
+		y = 0;
+		z = 0;
+		sujetado = false;
+	}
+	
+	int BrazoRobotico::getX(){
+		return x;
+	}
+	
+	int BrazoRobotico::getY(){
+		return y;
+	}
+	
+	int BrazoRobotico::getZ(){
+		return z;
+	}
+	
+	bool
 
 
